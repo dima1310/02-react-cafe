90d94a0ddbc55e6afed00875971ebcafcd02d07d
dima1310 <dimaponomarenko13101981@gmail.com>
2025-06-28 13:08:58 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/App/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+import "modern-normalize/modern-normalize.css";
